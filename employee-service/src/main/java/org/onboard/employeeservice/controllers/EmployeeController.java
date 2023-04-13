1376aa67c824875fdca41448693f09ffee380127
benjarmoun <93975590+benjarmoun@users.noreply.github.com>
2023-04-13 02:36:28 +0000

    updates

--- employee-service/src/main/java/org/onboard/employeeservice/controllers/EmployeeController.java
+++ employee-service/src/main/java/org/onboard/employeeservice/controllers/EmployeeController.java
@@ -1,26 +1,75 @@
 package org.onboard.employeeservice.controllers;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.onboard.employeeservice.dto.LeaveDto;
+import org.onboard.employeeservice.models.Leave;
+import org.onboard.employeeservice.services.ContractServiceImp;
+import org.onboard.employeeservice.services.EmployeeService;
+import org.onboard.employeeservice.services.LeaveRestClientService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 
 @RestController
 @RefreshScope
+@AllArgsConstructor
+@RequestMapping("employee")
 public class EmployeeController {
 
-    @Value("${global.params.p1}")
-    private String p1;
-    @Value("${global.params.p2}")
-    private String p2;
-    @Value("${employee.params.x}")
-    private String x;
-    @Value("${employee.params.y}")
-    private String y;
-    @GetMapping("/params")
-    public Map<String, String> params(){
-        return Map.of("p1",p1,"p2",p2,"x",x,"y",y);
+    EmployeeService employeeService;
+    LeaveRestClientService leaveRestClientService;
+
+    @GetMapping("employee") //to employee
+    public ResponseEntity<Object> getAccountByEmail(HttpServletRequest httpSerReq){
+        String email = employeeService.getEmail(httpSerReq);
+        return ResponseEntity.ok(employeeService.getEmployeeByEmail(email));
     }
+
+
+    @PostMapping("requestLeave")  //employee
+    public ResponseEntity<String> createLeaveRequest(@RequestBody LeaveDto leaveDto, HttpServletRequest httpSerReq){
+        leaveDto.setEmployeeId(employeeService.getEmpId(httpSerReq));
+        if(leaveRestClientService.addLeaveRequest(leaveDto).getStatusCode().value() == 200){
+            return ResponseEntity.status(200).body("Leave requested successfully");
+        }else
+            return ResponseEntity.status(400).body("An error has occured");
+    }
+
+    @GetMapping("LeaveSoldeEmployee")  //to employee
+    public ResponseEntity<Object> leaveSoldeEmployee(HttpServletRequest httpSerReq){
+        Long id = employeeService.getEmpId(httpSerReq);
+        return ResponseEntity.ok(leaveRestClientService.leaveSolde(id));
+    }
+
+    @GetMapping("usedLeaveDays")  //to employee
+    public ResponseEntity<Object> usedLeaveDaysByEmp(HttpServletRequest httpSerReq){
+        Long id = employeeService.getEmpId(httpSerReq);
+        return ResponseEntity.ok(leaveRestClientService.usedLeaveDaysByEmpId(id));
+    }
+
+    @GetMapping("employeeUpcomingLeaves")
+    public ResponseEntity<Object> upcomingLeavesByEmp(HttpServletRequest httpSerReq){
+        Long id = employeeService.getEmpId(httpSerReq);
+        List<Leave> leaves =  leaveRestClientService.getUpcomingLeavesByEmpId(id);
+        leaves.stream()
+                .forEach(leave -> leave.setEmployee(employeeService.getEmployeeById(leave.getEmployeeId())));
+        return ResponseEntity.ok(leaves);
+    }
+
+    @GetMapping("LeavesByEmployee")  //to employee
+    public ResponseEntity<Object> leavesByEmployee(HttpServletRequest httpSerReq){
+        Long id = employeeService.getEmpId(httpSerReq);
+        System.out.println("id"+id);
+        List<Leave> leaves =  leaveRestClientService.getLeavesByEmpId(id);
+        System.out.println(leaves);
+        leaves.stream()
+                .forEach(leave -> leave.setEmployee(employeeService.getEmployeeById(leave.getEmployeeId())));
+        return ResponseEntity.ok(leaves);
+    }
+
 }
--- employee-service/src/main/java/org/onboard/employeeservice/controllers/RhController.java
+++ employee-service/src/main/java/org/onboard/employeeservice/controllers/RhController.java
@@ -44,11 +44,6 @@ public class RhController {
         Employee employee = employeeService.getEmployeeById(id);
         return ResponseEntity.status(200).body(employee);
     }
-    @GetMapping("employee") //to employee
-    public ResponseEntity<Object> getAccountByEmail(HttpServletRequest httpSerReq){
-        String email = employeeService.getEmail(httpSerReq);
-        return ResponseEntity.ok(employeeService.getEmployeeByEmail(email));
-    }
 
     @PostMapping("/addEmployee")
     public ResponseEntity<String> addEmployee(@RequestBody EmployeeInputDto employeeInputDto) throws EmployeeEmailExistException, MissingFieldsException {
@@ -87,28 +82,10 @@ public class RhController {
         return ResponseEntity.ok(leave);
     }
 
-    @GetMapping("LeavesByEmployee")  //to employee
-    public ResponseEntity<Object> leavesByEmployee(HttpServletRequest httpSerReq){
-        Long id = employeeService.getEmpId(httpSerReq);
-        List<Leave> leaves =  leaveRestClientService.getLeavesByEmpId(id);
-        leaves.stream()
-                .forEach(leave -> leave.setEmployee(employeeService.getEmployeeById(leave.getEmployeeId())));
-        return ResponseEntity.ok(leaves);
-    }
-
     @GetMapping("LeavesByEmpId/{id}")
     public ResponseEntity<Object> leavesByEmpId(@PathVariable Long id){
         List<Leave> leave =  leaveRestClientService.getLeavesByEmpId(id);
         return ResponseEntity.ok(leave);
-    }
-
-    @GetMapping("employeeUpcomingLeaves")
-    public ResponseEntity<Object> upcomingLeavesByEmp(HttpServletRequest httpSerReq){
-        Long id = employeeService.getEmpId(httpSerReq);
-        List<Leave> leaves =  leaveRestClientService.getUpcomingLeavesByEmpId(id);
-        leaves.stream()
-                .forEach(leave -> leave.setEmployee(employeeService.getEmployeeById(leave.getEmployeeId())));
-        return ResponseEntity.ok(leaves);
     }
 
     @GetMapping("noContractEmployees")
@@ -121,15 +98,6 @@ public class RhController {
     public ResponseEntity<Object> leaves() {
         List<Leave> leave = leaveRestClientService.getAllLeaves();
         return ResponseEntity.ok(leave);
-    }
-
-    @PostMapping("requestLeave")  //employee
-    public ResponseEntity<String> createLeaveRequest(@RequestBody LeaveDto leaveDto,HttpServletRequest httpSerReq){
-        leaveDto.setEmployeeId(employeeService.getEmpId(httpSerReq));
-        if(leaveRestClientService.addLeaveRequest(leaveDto).getStatusCode().value() == 200){
-            return ResponseEntity.status(200).body("Leave requested successfully");
-        }else
-            return ResponseEntity.status(400).body("An error has occured");
     }
 
     @GetMapping("pendingLeaveRequests")
@@ -168,21 +136,9 @@ public class RhController {
         return ResponseEntity.ok(leave);
     }
 
-    @GetMapping("LeaveSoldeEmployee")  //to employee
-    public ResponseEntity<Object> leaveSoldeEmployee(HttpServletRequest httpSerReq){
-        Long id = employeeService.getEmpId(httpSerReq);
-        return ResponseEntity.ok(leaveRestClientService.leaveSolde(id));
-    }
-
     @GetMapping("LeaveSoldeByEmpId/{id}")
     public ResponseEntity<Object> leaveSoldeByEmployeeId(@PathVariable Long id){
         return ResponseEntity.ok(leaveRestClientService.leaveSolde(id));
-    }
-
-    @GetMapping("usedLeaveDays")  //to employee
-    public ResponseEntity<Object> usedLeaveDaysByEmp(HttpServletRequest httpSerReq){
-        Long id = employeeService.getEmpId(httpSerReq);
-        return ResponseEntity.ok(leaveRestClientService.usedLeaveDaysByEmpId(id));
     }
 
     @GetMapping("usedLeaveDaysByEmpId/{id}")
